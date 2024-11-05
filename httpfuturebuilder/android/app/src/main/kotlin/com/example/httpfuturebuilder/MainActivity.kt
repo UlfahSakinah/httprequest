c53cb8ac73f701843064ca07551130e29e7e1cb8
httprequest <ulfahsakinah@unipdu.ac.id>
2024-11-06 01:38:36 +0700

    Http Request

---
+++ httpfuturebuilder/android/app/src/main/kotlin/com/example/httpfuturebuilder/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.httpfuturebuilder
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ httpmodel/android/app/src/main/kotlin/com/example/httpmodel/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.httpmodel
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
